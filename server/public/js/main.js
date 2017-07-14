70649b2a84393a346646bc4cedb71d25418ee97a
James Skinner <spiralx@gmail.com>
2017-07-14 19:20:55 +0100

    Added note send form

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,5 +1,41 @@
 
+
+function formData($el) {
+  return $el
+    .serializeArray()
+    .reduce((acc, { name, value }) => Object.assign(acc, { [name]: value }), {})
+}
+
+
 jQuery(function ($) {
   console.info(`jQuery loaded!`)
   console.dir($)
+
+  const $form = $('#note-form')
+
+  $('#note-send').click(function(ev) {
+    const noteData = formData($form)
+
+    console.dir(noteData)
+
+    $(this).addClass('disabled')
+
+    $.post('/note', noteData)
+      .done(data => {
+        console.group(`/note?${$('#note-form').serialize()}`)
+        console.dir(noteData)
+        console.dir(data)
+        console.groupEnd()
+      })
+      .fail((jqXHR, textStatus, errorThrown) => {
+        console.group(`/note?${$('#note-form').serialize()}`)
+        console.dir(noteData)
+        console.warn(`${textStatus}: %o`, errorThrown)
+        console.dir(jqXHR)
+        console.groupEnd()
+      })
+      .always(() => {
+        $(this).removeClass('disabled')
+      })
+  })
 })
